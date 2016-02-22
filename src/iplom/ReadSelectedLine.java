fe883310ce7116850e50b24c36b1e9ecaf9d0d80
Chang Liu <edghklj@esekilxxen1711.rnd.ericsson.se>
2016-02-22 11:19:27 +0100

    read selected line

---
+++ src/iplom/ReadSelectedLine.java
@@ -0,0 +1,31 @@
+/**
+ * Class: ReadSelectedLine
+ * 
+ * @author ERICSSON/edghklj (Chang Liu)
+ * 
+ * Created AT 2016-02-22
+ * 
+ */
+package iplom;
+
+public class ReadSelectedLine {
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
