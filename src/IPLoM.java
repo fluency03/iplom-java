cea31edb3b31251a2147723cad47ca3a8191c8af
fluency03 <fluency.03@gmail.com>
2016-02-22 09:42:14 +0100

    src

---
+++ src/IPLoM.java
@@ -0,0 +1,47 @@
+/**
+ * Class: IPLoM
+ * 
+ * Based on the log mining algorithms published on the following papers:
+ * 
+ * [1] Adetokunbo AO Makanju, A Nur Zincir-Heywood, and Evangelos E Milios. Clustering 
+ * event logs using iterative partitioning. In Proceedings of the 15th ACM SIGKDD international 
+ * conference on Knowledge discovery and data mining, pages 1255–1264. ACM, 2009.
+ * 
+ * [2] Adetokunbo Makanju, A Nur Zincir-Heywood, and Evangelos E Milios. A lightweight
+ * algorithm for message type extraction in system application logs. Knowledge and Data
+ * Engineering, IEEE Transactions on, 24(11):1921–1936, 2012.
+ * 
+ * @author edghklj
+ *
+ * Created AT 2016-02-22
+ *
+ */
+
+
+public class IPLoM {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
