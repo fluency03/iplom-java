8f491295afc0f2606b4110ee935aa0ddc92eb1e2
Chang Liu <fluency.03@gmail.com>
2016-02-24 19:15:05 +0100

    Pair

---
+++ src/iplom/Pair.java
@@ -0,0 +1,39 @@
+/**
+ * Class: Pair
+ * @author edghklj
+ *
+ * @param <L>
+ * @param <R>
+ */
+
+package iplom;
+
+public class Pair<L,R> {
+
+  private L left;
+  private R right;
+
+  public Pair(L left, R right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public L getLeft() { return left; }
+  public R getRight() { return right; }
+  
+  public void setLeft(L left) { this.left = left; }
+  public void setRight(R right) { this.right = right; }
+
+  @Override
+  public int hashCode() { return left.hashCode() ^ right.hashCode(); }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Pair)) return false;
+    @SuppressWarnings("rawtypes")
+    Pair pairo = (Pair) o;
+    return this.left.equals(pairo.getLeft()) &&
+           this.right.equals(pairo.getRight());
+  }
+
+}
