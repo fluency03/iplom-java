c17861d7a0982bc34b04eb02e1c8fe08a8d86a36
Chang Liu <fluency.03@gmail.com>
2016-02-25 10:39:51 +0100

    Pair class

--- src/iplom/Pair.java
+++ src/iplom/Pair.java
@@ -2,17 +2,23 @@
  * Class: Pair
  * @author edghklj
  *
- * @param <L>
- * @param <R>
+ * @param <L> type of left
+ * @param <R> type of right
  */
 
 package iplom;
 
 public class Pair<L,R> {
 
+  /**
+   * Members: L left and R right
+   */
   private L left = null;
   private R right = null;
 
+  /**
+   * Constructors
+   */
   public Pair() {}
   
   public Pair(L left, R right) {
@@ -20,20 +26,34 @@ public class Pair<L,R> {
     this.right = right;
   }
 
+  /**
+   * Get the members
+   * @return
+   */
   public L getLeft() { return left; }
   public R getRight() { return right; }
   
+  /**
+   * Set the members
+   */
   public void setLeft(L left) { this.left = left; }
   public void setRight(R right) { this.right = right; }
 
+  /**
+   * (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
   @Override
   public int hashCode() { return left.hashCode() ^ right.hashCode(); }
 
+  /**
+   * check whether it si equal to another Object
+   * Based on its type and two members
+   */
   @Override
   public boolean equals(Object o) {
     if (!(o instanceof Pair)) return false;
-    @SuppressWarnings("rawtypes")
-    Pair pairo = (Pair) o;
+    Pair<?, ?> pairo = (Pair<?, ?>) o;
     return this.left.equals(pairo.getLeft()) &&
            this.right.equals(pairo.getRight());
   }
