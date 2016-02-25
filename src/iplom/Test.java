8f0b7cf8e329eaac8fbb58c50634b1b386a541c4
Chang Liu <fluency.03@gmail.com>
2016-02-25 15:04:54 +0100

    test

--- src/iplom/Test.java
+++ src/iplom/Test.java
@@ -13,12 +13,21 @@ public class Test {
    * @param args
    */
   public static void main(String[] args) {
+    
+    /* ------------- Start time of the program ------------- */
+    long startTime = System.currentTimeMillis();
+    /* ------------- Start time of the program ------------- */
+    
+    /* ---------------- Output the printed to a file  ---------------- */
     try {
       System.setOut(new PrintStream(new FileOutputStream("/home/edghklj/workspace/github/iplom-java/output.txt")));
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     } 
+    /* ---------------- Output the printed to a file  ---------------- */
+    
+    
     
     String fileName = "/home/edghklj/workspace/github/messages.1";
     IPLoM logAnalyzer = new IPLoM(fileName);
@@ -29,8 +38,19 @@ public class Test {
     //logAnalyzer.printSizePartition();
     logAnalyzer.partitionByTokenPosition();
     
-    out.println("The analyzed log file: " + logAnalyzer.returnFile());
     
+    
+    /* ---------------- Analyzed file name  ---------------- */
+    out.println("The analyzed log file: " + logAnalyzer.returnFile());
+    /* ---------------- Analyzed file name  ---------------- */
+    
+    /* -------------- End time of the program -------------- */
+    long endTime   = System.currentTimeMillis();
+    /* -------------- End time of the program -------------- */
+    
+    /* ------------- Print out the running time ------------- */
+    out.println("Running Time: " + (endTime - startTime) + " ms");
+    /* ------------- Print out the running time ------------- */
   }
 
 }
