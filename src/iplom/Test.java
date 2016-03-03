02c60c25a4cc5792b759355c5422f2e3f11e1d8a
Chang Liu <fluency.03@gmail.com>
2016-03-03 14:15:02 +0100

    test stringtokenizer

--- src/iplom/Test.java
+++ src/iplom/Test.java
@@ -6,6 +6,7 @@ package iplom;
 
 import java.io.*;
 //import java.util.*;
+import java.util.StringTokenizer;
 
 import static java.lang.System.out;
 
@@ -16,23 +17,23 @@ public class Test {
    */
   public static void main(String[] args) {
     
-    /* ------------- Start time of the program ------------- */
+    /* --------------- Start time of the program --------------- */
     long startTime = System.currentTimeMillis();
-    /* ------------- Start time of the program ------------- */
+    /* --------------- Start time of the program --------------- */
     
-    /* ---------------- Output the printed to a file  ---------------- */
+    /* ----------------- Output the printed to a file  ----------------- */
     try {
       System.setOut(new PrintStream(new FileOutputStream("/home/edghklj/workspace/github/iplom-java/output.txt")));
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     } 
-    /* ---------------- Output the printed to a file  ---------------- */
+    /* ----------------- Output the printed to a file  ----------------- */
     
     
     
     
-    
+    /* ------------------------- Main program  ------------------------- */
     String fileName = "/home/edghklj/workspace/github/messages.1";
     IPLoM logAnalyzer = new IPLoM(fileName);
     
@@ -41,23 +42,36 @@ public class Test {
     //logAnalyzer.partitionByTokenPosition();
     //logAnalyzer.partitionByTokenBijection();
     logAnalyzer.dicoverLogTemplate();
+    /* ------------------------- Main program  ------------------------- */
     
     
     
     
-    
-    /* ---------------- Analyzed file name  ---------------- */
+    /* ------------------ Analyzed file name  ------------------ */
     out.println("\nThe analyzed log file: " + logAnalyzer.returnFile());
-    /* ---------------- Analyzed file name  ---------------- */
+    /* ------------------ Analyzed file name  ------------------ */
     
-    /* -------------- End time of the program -------------- */
+    /* ---------------- End time of the program ---------------- */
     long endTime   = System.currentTimeMillis();
-    /* -------------- End time of the program -------------- */
+    /* ---------------- End time of the program ---------------- */
     
-    /* ------------- Print out the running time ------------- */
+    /* -------------- Print out the running time --------------- */
     out.println("\nRunning Time: " + (endTime - startTime) + " ms");
-    /* ------------- Print out the running time ------------- */
+    /* -------------- Print out the running time --------------- */
     
+    
+    
+    /* --------------------- For testing ----------------------- */
+    String str = "asfe[[asdq] fwev==qw cq:we we'f'we  /asad/qwd/asdq asd|111";
+    String delimiter = " []=:()/|\'\"";
+    
+    StringTokenizer tokens = new StringTokenizer(str, delimiter, true);
+    while (tokens.hasMoreElements()) {
+      out.println(tokens.nextToken());
+    }
+    /* --------------------- For testing ----------------------- */
+    
+
   }
 
 }
